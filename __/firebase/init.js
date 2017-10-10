a6da078b3a3c3e86af6d0f7845bf9de5d33d20a3
Zain Fathoni <zain.fathoni@gmail.com>
2017-10-11 05:36:47 +0700

    init: Firebase initialization configuration

---
+++ __/firebase/init.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD90F0mvUoeCCQQZPmJGyLHVwrwyVtw6pw",
+  authDomain: "friendlyeatsweb.firebaseapp.com",
+  databaseURL: "https://friendlyeatsweb.firebaseio.com",
+  projectId: "friendlyeatsweb",
+  storageBucket: "friendlyeatsweb.appspot.com",
+  messagingSenderId: "9483753782"
+};
+firebase.initializeApp(config);
